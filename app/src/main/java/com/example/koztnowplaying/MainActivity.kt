bf4682a50fb00e48a015cd54b5371ab28aed53b5
Tony <1022162+short-y@users.noreply.github.com>
2025-06-26 12:26:20 -0700

    fix: Add missing imports for verticalScroll and rememberScrollState

--- app/src/main/java/com/example/koztnowplaying/MainActivity.kt
+++ app/src/main/java/com/example/koztnowplaying/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
