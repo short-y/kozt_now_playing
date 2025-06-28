7cc507b21b54c28309b100c8666525b151beea10
Tony <1022162+short-y@users.noreply.github.com>
2025-06-28 08:11:02 -0700

    fix: add missing imports

--- app/src/main/java/com/example/koztnowplaying/MainActivity.kt
+++ app/src/main/java/com/example/koztnowplaying/MainActivity.kt
@@ -29,11 +29,13 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.min
